412906ba526b4cbe9cf25acd0be992173ebf847d
Yoshitomo Minami <minami530@gmail.com>
2019-12-21 19:56:02 +0900

    Generate vs projects

---
+++ libs/curlcpp/curlcppConfig.cmake
@@ -0,0 +1,5 @@
+# curlcppConfig.cmake - Configuration file for external projects.
+
+set(CURLCPP_INCLUDE_DIRS "C:/dev/ttt/The-Modern-Cpp-Challenge/libs/curlcpp/include")
+set(CURLCPP_LIBRARY_DIRS "C:/dev/ttt/The-Modern-Cpp-Challenge/libs/curlcpp/src")
+set(CURLCPP_LIBRARIES "$<TARGET_FILE:>")
